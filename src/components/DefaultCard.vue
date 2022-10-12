298b8c94c624f4b713931518ff9f70dc6abc39b9
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-12 20:19:01 +0300

    creating default card component

---
+++ src/components/DefaultCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card color="white" rounded="xl" elevation="6" width="400px" class="ma-1">
+    <v-card-title class="justify-center">
+      {{ title }}
+    </v-card-title>
+    <v-divider></v-divider>
+    <slot></slot>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "DefaultCard",
+
+  props: {
+    title: String,
+  },
+};
+</script>
