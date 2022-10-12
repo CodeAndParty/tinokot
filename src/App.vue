3413f53c81c524cfd0f13842cd98471d2945f4c3
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-12 12:57:16 +0300

    creating the main app bar and the navigation-drawer

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="white" clipped-right>
+      <v-img
+        alt="Tinokot Logo"
+        class="shrink ml-1"
+        src="./assets/logo.svg"
+        width="50"
+      />
+      <v-app-bar-title>תינוקות</v-app-bar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-icon>mdi-account</v-icon>
+      <span>אלכסנדר דרפקין</span>
     </v-app-bar>
+
+    <v-navigation-drawer app clipped right permanent color="primary">
+      <OrganizationalStructure></OrganizationalStructure>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
