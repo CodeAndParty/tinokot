ff8d2c1d9d069449d181366b27af81ff24b0c6ed
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-12 12:56:21 +0300

    setting vuetify to support rtl(for hebrew) + creating the default theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,20 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  rtl: true,
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: "#282949",
+        secondary: "#5271ff",
+        accent: "#595a7f",
+        info: "#ffffff",
+        warning: "#ffffff",
+        error: "#ffaa9d",
+        success: "#92eed5",
+      },
+    },
+  },
+});
