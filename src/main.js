b16b4396a64496f574a2ba4a3b4522be91fd90c4
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 02:41:57 +0300

    installing and using fragment plugin to wrap elements inside template without root component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 import "./plugins/axios";
+import "./plugins/fragment";
 import "./plugins/date";
 
 Vue.config.productionTip = false;
