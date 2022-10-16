b9c86e198759373406c85555fb5e8feb7b83fc0b
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 02:12:19 +0300

    creating base dialog component for use for basic dialogs

---
+++ src/components/BaseDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="400"
+      content-class="elevation-0 rounded-xl"
+    >
+      <BaseCard width="auto">
+        <template #title>
+          <v-spacer></v-spacer>
+          <slot name="title" />
+          <v-spacer></v-spacer>
+
+          <v-btn icon color="secondary" @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </template>
+
+        <slot />
+      </BaseCard>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import BaseCard from "./BaseCard.vue";
+
+export default {
+  name: "BaseDialog",
+
+  data: () => ({
+    dialog: false,
+  }),
+
+  mounted() {
+    this.$root.$on("openDialog", () => (this.dialog = true));
+  },
+
+  components: { BaseCard },
+};
+</script>
