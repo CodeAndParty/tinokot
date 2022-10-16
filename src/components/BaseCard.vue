00d9c61aae4310530ab4db4b6fc1b12ceebed21f
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 02:47:37 +0300

    emiting click event every time the base card is clicked (used to work wtih dialogs)

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-card color="white" rounded="xl" elevation="6" :width="width" class="ma-1">
+  <v-card
+    color="white"
+    rounded="xl"
+    elevation="6"
+    :width="width"
+    class="ma-1"
+    @click.native="$emit('click')"
+  >
     <v-card-title class="justify-center font-weight-bold primary--text">
       <slot name="title" />
     </v-card-title>
