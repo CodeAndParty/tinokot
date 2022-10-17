0765b0a0cb6938baaf2e1d97b4fe14ab98aec4d9
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 23:26:23 +0300

    creating seperate files for proficiencty bonus dialog and for proficiency bonus card and one component to combine them together

---
+++ src/components/ProficiencyBonusCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <BaseCard is-clickable>
+    <template #title> גמו"ש </template>
+
+    <div v-for="n in proficiencyBonus?.type === 'A' ? 1 : 2" :key="n">
+      <div
+        class="px-5"
+        :class="{
+          'text-center mt-10': proficiencyBonus?.type === 'A',
+          'd-flex mt-4': proficiencyBonus?.type === 'AB',
+        }"
+      >
+        <p class="mb-0">
+          <span class="text-h6 font-weight-bold">{{
+            n === 1 ? "גמוש א" : "גמוש ב"
+          }}</span>
+          <br />
+          <span>
+            {{
+              $formatDateString(
+                n === 1
+                  ? proficiencyBonus?.firstDate
+                  : proficiencyBonus?.secondDate
+              )
+            }}
+          </span>
+        </p>
+        <v-spacer v-if="proficiencyBonus?.type === 'AB'"></v-spacer>
+        <v-icon
+          v-if="
+            !$isDateStringBiggerThanToday(
+              n === 1
+                ? proficiencyBonus?.firstDate
+                : proficiencyBonus?.secondDate
+            )
+          "
+          x-large
+          color="success"
+        >
+          mdi-check
+        </v-icon>
+        <v-icon v-else x-large color="error"> mdi-close </v-icon>
+      </div>
+      <v-divider
+        v-if="proficiencyBonus?.type === 'AB' && n === 1"
+        class="my-5"
+      ></v-divider>
+    </div>
+  </BaseCard>
+</template>
+
+<script>
+import BaseCard from "./BaseCard.vue";
+export default {
+  name: "ProficiencyBonusCard",
+
+  components: { BaseCard },
+
+  computed: {
+    proficiencyBonus() {
+      return this.$store.getters.proficiencyBonus;
+    },
+  },
+};
+</script>
---
+++ src/components/ProficiencyBonusDialog.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseDialog>
+    <template #title> גמו"ש </template>
+  </BaseDialog>
+</template>
+
+<script>
+import BaseDialog from "./BaseDialog.vue";
+export default {
+  name: "ProficiencyBonus",
+  components: { BaseDialog },
+};
+</script>
