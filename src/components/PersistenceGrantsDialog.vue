dd13e2f762d83df0afab63f2a0de6f0bc0c39f21
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 19:54:33 +0300

    creating the persistenceGrants dialog component

--- src/components/PersistenceGrantsDialog.vue
+++ src/components/PersistenceGrantsDialog.vue
@@ -1,7 +1,28 @@
 <template>
   <BaseDialog>
     <template #title> מענקי התמדה </template>
-    <div>test</div>
+    <v-progress-linear
+      value="100"
+      color="primary"
+      class="mt-5"
+    ></v-progress-linear>
+    <v-card-text class="d-flex justify-space-between">
+      <div v-for="item in timeline" :key="item.name" class="text-center ma-5">
+        <span>
+          <span>{{ item.name }}</span>
+        </span>
+        <br />
+        <span
+          class="font-weight-bold"
+          :class="{
+            'success--text': !$isDateStringBiggerThanToday(item.date),
+            'error--text': $isDateStringBiggerThanToday(item.date),
+          }"
+        >
+          {{ $formatDateString(item.date) }}
+        </span>
+      </div>
+    </v-card-text>
   </BaseDialog>
 </template>
 
@@ -14,8 +35,31 @@ export default {
   components: { BaseDialog },
 
   computed: {
-    persistenceGrantsDates() {
-      return this.$store.getters.persistenceGrantsDates;
+    timeline() {
+      const persistenceGrantsDates =
+        this.$store.getters.persistenceGrantsDates?.slice(0);
+      persistenceGrantsDates.push(new Date().toLocaleDateString());
+      persistenceGrantsDates.sort((dateA, dateB) => {
+        return new Date(dateA) - new Date(dateB);
+      });
+
+      const timeline = [];
+      let persistenceGrantCounter = 1;
+      persistenceGrantsDates.forEach((date) => {
+        if (!this.$isDateStringToday(date)) {
+          timeline.push({
+            name: `מענק ${persistenceGrantCounter}`,
+            date: date,
+          });
+          persistenceGrantCounter++;
+        } else {
+          timeline.push({
+            name: "היום",
+            date: date,
+          });
+        }
+      });
+      return timeline;
     },
   },
 };
