df6ec9697e70c2dc8ba5272e0a06a3d0d9680c1f
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 17:46:29 +0300

    making the persistence grands carousel and dialog self closiong components

--- src/components/PersistenceGrants.vue
+++ src/components/PersistenceGrants.vue
@@ -1,7 +1,7 @@
 <template>
   <fragment>
-    <PersistenceGrantsCarousel></PersistenceGrantsCarousel>
-    <PersistenceGrantsDialog></PersistenceGrantsDialog>
+    <PersistenceGrantsCarousel />
+    <PersistenceGrantsDialog />
   </fragment>
 </template>
 
