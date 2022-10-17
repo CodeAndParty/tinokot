00d0677549658e852dfe67bb6a3f99e869cf4f2f
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-17 03:09:03 +0300

    dividing the persistence grants to carousel and dialog and one component to combine them, and making them work (the dialog is not ready yet)

--- src/components/PersistenceGrants.vue
+++ src/components/PersistenceGrants.vue
@@ -1,50 +1,15 @@
 <template>
-  <BaseCard>
-    <template #title>מענקי התמדה</template>
-    <v-carousel hide-delimiters height="fit-content" :continuous="false">
-      <v-carousel-item
-        v-for="(date, index) in persistenceGrantsDates"
-        :key="index"
-      >
-        <div class="text-center">
-          <v-icon x-large color="accent">mdi-piggy-bank</v-icon>
-          <v-card-text>
-            <p>
-              <span class="text-h6">מענק {{ index + 1 }}</span>
-              <br />
-              <span class="font-weight-bold">
-                {{ $formatDateString(date) }}
-              </span>
-            </p>
-            <v-icon
-              v-if="!$isDateStringBiggerThanToday(date)"
-              x-large
-              color="success"
-            >
-              mdi-check
-            </v-icon>
-            <v-icon v-else x-large color="error"> mdi-close </v-icon>
-          </v-card-text>
-        </div>
-      </v-carousel-item>
-    </v-carousel>
-  </BaseCard>
+  <fragment>
+    <PersistenceGrantsCarousel></PersistenceGrantsCarousel>
+    <PersistenceGrantsDialog></PersistenceGrantsDialog>
+  </fragment>
 </template>
 
 <script>
-import BaseCard from "./BaseCard.vue";
-
+import PersistenceGrantsCarousel from "./PersistenceGrantsCarousel.vue";
+import PersistenceGrantsDialog from "./PersistenceGrantsDialog.vue";
 export default {
   name: "PersistenceGrants",
-
-  components: { BaseCard },
-
-  computed: {
-    persistenceGrantsDates() {
-      return this.$store.getters.persistenceGrantsDates;
-    },
-  },
-
-  methods: {},
+  components: { PersistenceGrantsCarousel, PersistenceGrantsDialog },
 };
 </script>
---
+++ src/components/PersistenceGrantsDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseDialog>
+    <template #title> מענקי התמדה </template>
+    <div>test</div>
+  </BaseDialog>
+</template>
+
+<script>
+import BaseDialog from "./BaseDialog.vue";
+
+export default {
+  name: "PersistenceGrantsDialog",
+
+  components: { BaseDialog },
+
+  computed: {
+    persistenceGrantsDates() {
+      return this.$store.getters.persistenceGrantsDates;
+    },
+  },
+};
+</script>
