5b532b68746a8e098892d3ef9c14d4e304eb1014
Alexander Drapkin <alexander.drapkin@outlook.com>
2022-10-25 18:44:21 +0300

    creating persistence grants folder with 4 compomenent dialog,card carousel and timeline and remooving the name property from all of them, updating the soldier view to have the card and dialog

---
+++ src/components/PersistenceGrants/PersistenceGrantsCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <BaseCard isClickable @click="$root.$emit('open-persistence-grants-dialog')">
+    <template #title>מענקי התמדה</template>
+    <PersistenceGrantsCarousel></PersistenceGrantsCarousel>
+  </BaseCard>
+</template>
+
+<script>
+import BaseCard from "../BaseCard.vue";
+import PersistenceGrantsCarousel from "./PersistenceGrantsCarousel.vue";
+export default { components: { BaseCard, PersistenceGrantsCarousel } };
+</script>
---
+++ src/components/PersistenceGrants/PersistenceGrantsDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseDialog ref="dialog">
+    <template #title>ציר זמן מענקי התמדה</template>
+    <PersistenceGrantsTimeline />
+  </BaseDialog>
+</template>
+
+<script>
+import BaseDialog from "../BaseDialog.vue";
+import PersistenceGrantsTimeline from "./PersistenceGrantsTimeline.vue";
+
+export default {
+  components: { BaseDialog, PersistenceGrantsTimeline },
+
+  mounted() {
+    this.$root.$on(
+      "open-persistence-grants-dialog",
+      this.$refs.dialog.openDialog
+    );
+  },
+};
+</script>
